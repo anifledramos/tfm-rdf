52c1d712115dc8191f831f6714e44bffd12d7aba
Delfina Ramos <delfina.ramos@udc.es>
2021-06-10 08:35:00 +0200

    Increased Java heap space

--- hdt-jni/hdt-java-cli/bin/rdf2hdt.sh
+++ hdt-jni/hdt-java-cli/bin/rdf2hdt.sh
@@ -3,7 +3,7 @@
 source `dirname $0`/javaenv.sh
 
 #$JAVA $JAVA_OPTIONS -cp $CP:$CLASSPATH -XX:NewRatio=1 -XX:SurvivorRatio=9 org.rdfhdt.hdt.tools.RDF2HDT $* 
-MAVEN_OPTS=-Xmx12g
+MAVEN_OPTS=-Xmx64g
 export MAVEN_OPTS
 
 mvn exec:java -Dexec.mainClass="org.rdfhdt.hdt.tools.RDF2HDT" -Dexec.args="$*"
