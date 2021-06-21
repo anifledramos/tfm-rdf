519ea9a28367a0f06187d45e79c26fdb6f54f502
anifledramos <anifledramos@gmail.com>
2021-06-21 02:49:21 +0200

    Fixed issue with FourSectionDictionaryBig

---
+++ hdt-jni/hdt-java-core/src/main/java/org/rdfhdt/hdt/dictionary/impl/JNIDictionaryBig.java
@@ -0,0 +1,180 @@
+package org.rdfhdt.hdt.dictionary.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.rdfhdt.hdt.dictionary.DictionarySectionPrivate;
+import org.rdfhdt.hdt.dictionary.TempDictionary;
+import org.rdfhdt.hdt.dictionary.impl.section.DictionarySectionFactory;
+import org.rdfhdt.hdt.dictionary.impl.section.JNIDictionarySection;
+import org.rdfhdt.hdt.exceptions.IllegalFormatException;
+import org.rdfhdt.hdt.hdt.HDTVocabulary;
+import org.rdfhdt.hdt.header.Header;
+import org.rdfhdt.hdt.listener.ProgressListener;
+import org.rdfhdt.hdt.options.ControlInfo;
+import org.rdfhdt.hdt.options.HDTOptions;
+import org.rdfhdt.hdt.options.ControlInfo.Type;
+import org.rdfhdt.hdt.util.io.CountInputStream;
+import org.rdfhdt.hdt.util.listener.IntermediateListener;
+
+public class JNIDictionaryBig extends BaseDictionary { 
+	
+	public JNIDictionaryBig(HDTOptions spec, DictionarySectionPrivate s, DictionarySectionPrivate p, DictionarySectionPrivate o, DictionarySectionPrivate sh) {
+	super(spec);
+	this.subjects = s;
+	this.predicates = p;
+	this.objects = o;
+	this.shared = sh;
+	}
+	
+	public JNIDictionaryBig(HDTOptions spec) {
+		super(spec);
+		System.out.println("Free memory dictionary init (bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+		subjects = new JNIDictionarySection(spec);
+		System.out.println("Free memory dict s(bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+		predicates = new JNIDictionarySection(spec);
+		System.out.println("Free memory dict p(bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+		objects = new JNIDictionarySection(spec);
+		System.out.println("Free memory dict o(bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+		shared = new JNIDictionarySection(spec);
+		System.out.println("Free memory dict shr (bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+	}
+	
+	@Override
+	public void load(TempDictionary other, ProgressListener listener) throws IOException {
+		IntermediateListener iListener = new IntermediateListener(listener);
+		System.out.println("Max memory (bytes): " + 
+				  Runtime.getRuntime().maxMemory());
+		System.out.println("Free memory dict before subjects(bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+		subjects.load(other.getSubjects(), iListener, "subjects");
+		System.out.println("Max memory (bytes): " + 
+				  Runtime.getRuntime().maxMemory());
+		System.out.println("Free memory dict before predicates(bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+		predicates.load(other.getPredicates(), iListener, "predicates");
+		System.out.println("Max memory (bytes): " + 
+				  Runtime.getRuntime().maxMemory());
+		System.out.println("Free memory dict before objects(bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+		objects.load(other.getObjects(), iListener, "objects");
+		System.out.println("Max memory (bytes): " + 
+				  Runtime.getRuntime().maxMemory());
+		System.out.println("Free memory dict before shared(bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+		shared.load(other.getShared(), iListener, "shared");
+	}
+	
+	@Override
+	public void load(String filename, ControlInfo ci, ProgressListener listener) throws IOException {
+		if(ci.getType()!=ControlInfo.Type.DICTIONARY) {
+			throw new IllegalFormatException("Trying to read a dictionary section, but was not dictionary.");
+		}
+		
+		IntermediateListener iListener = new IntermediateListener(listener);
+		subjects.load(filename+".dic.s", iListener);
+		predicates.load(filename+".dic.p", iListener);
+		objects.load(filename+".dic.ob", iListener);
+		shared.load(filename+".dic.shr", iListener);
+	}
+	
+	@Override
+	public void load(InputStream input, String filename, ControlInfo ci, ProgressListener listener) throws IOException {
+		if(ci.getType()!=ControlInfo.Type.DICTIONARY) {
+			throw new IllegalFormatException("Trying to read a dictionary section, but was not dictionary.");
+		}
+		
+		IntermediateListener iListener = new IntermediateListener(listener);
+		subjects.load(input, iListener);
+		predicates.load(input, iListener);
+		objects.load(input, iListener);
+		shared.load(input, iListener);		
+	}
+	
+	@Override
+	public void mapFromFile(CountInputStream in, File f, ProgressListener listener) throws IOException {
+	}
+	
+	@Override
+	public void save(OutputStream output, ControlInfo ci, ProgressListener listener) throws IOException {
+		ci.setType(Type.DICTIONARY);
+		ci.setFormat(HDTVocabulary.DICTIONARY_TYPE_JNI);
+		ci.setInt("elements", this.getNumberOfElements());
+		ci.save(output);
+
+		IntermediateListener iListener = new IntermediateListener(listener);
+		shared.save(output, iListener);
+		subjects.save(output, iListener);
+		predicates.save(output, iListener);
+		objects.save(output, iListener);
+
+	}
+	
+	@Override
+	public void save(OutputStream output, String filename, ControlInfo ci, ProgressListener listener)
+			throws IOException {
+		ci.setType(Type.DICTIONARY);
+		ci.setFormat(HDTVocabulary.DICTIONARY_TYPE_JNI);
+		ci.setInt("elements", this.getNumberOfElements());
+		ci.save(output);
+
+		IntermediateListener iListener = new IntermediateListener(listener);
+		shared.save(output, filename+".dic.shr", iListener);
+		subjects.save(output, filename+".dic.s", iListener);
+		predicates.save(output, filename+".dic.p", iListener);
+		objects.save(output, filename+".dic.ob", iListener);
+		
+	}
+	
+	@Override
+	public void load(InputStream input, ControlInfo ci, ProgressListener listener) throws IOException {
+		if(ci.getType()!=ControlInfo.Type.DICTIONARY) {
+			throw new IllegalFormatException("Trying to read a dictionary section, but was not dictionary.");
+		}
+
+		IntermediateListener iListener = new IntermediateListener(listener);
+		shared = DictionarySectionFactory.loadFrom(input, iListener);		
+		subjects = DictionarySectionFactory.loadFrom(input, iListener);
+		predicates = DictionarySectionFactory.loadFrom(input, iListener);
+		objects = DictionarySectionFactory.loadFrom(input, iListener);
+	}
+	
+	@Override
+	public void populateHeader(Header header, String rootNode) {
+		header.insert(rootNode, HDTVocabulary.DICTIONARY_TYPE, HDTVocabulary.DICTIONARY_TYPE_JNI);
+		header.insert(rootNode, HDTVocabulary.DICTIONARY_NUMSHARED, getNshared());
+		header.insert(rootNode, HDTVocabulary.DICTIONARY_SIZE_STRINGS, size());
+	}
+	
+	@Override
+	public String getType() {
+		return HDTVocabulary.DICTIONARY_TYPE_JNI;
+	}
+	@Override
+	public void close() throws IOException {
+		if (shared!=null) shared.close();
+		if (subjects!=null) subjects.close();
+		if (predicates!=null) predicates.close();
+		if (objects!=null) objects.close();
+	}
+
+	@Override
+	public boolean singleFileStorage(String type) {
+		if (type==HDTVocabulary.DICTIONARY_TYPE_JNI) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	
+		
+}
+
---
+++ hdt-jni/hdt-java-core/src/main/java/org/rdfhdt/hdt/dictionary/impl/section/JNIDictionarySectionBig.java
@@ -0,0 +1,302 @@
+/**
+ * File: $HeadURL: https://hdt-java.googlecode.com/svn/trunk/hdt-java/src/org/rdfhdt/hdt/dictionary/impl/section/PFCDictionarySection.java $
+ * Revision: $Rev: 201 $
+ * Last modified: $Date: 2013-04-17 23:40:20 +0100 (mié, 17 abr 2013) $
+ * Last modified by: $Author: mario.arias $
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contacting the authors:
+ *   Mario Arias:               mario.arias@deri.org
+ *   Javier D. Fernandez:       jfergar@infor.uva.es
+ *   Miguel A. Martinez-Prieto: migumar2@infor.uva.es
+ *   Alejandro Andres:          fuzzy.alej@gmail.com
+ */
+
+package org.rdfhdt.hdt.dictionary.impl.section;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Iterator;
+
+import org.rdfhdt.hdt.compact.integer.VByte;
+import org.rdfhdt.hdt.compact.sequence.SequenceLog64Big;
+import org.rdfhdt.hdt.dictionary.DictionarySectionPrivate;
+import org.rdfhdt.hdt.dictionary.TempDictionarySection;
+import org.rdfhdt.hdt.exceptions.CRCException;
+import org.rdfhdt.hdt.exceptions.IllegalFormatException;
+import org.rdfhdt.hdt.listener.ProgressListener;
+import org.rdfhdt.hdt.options.HDTOptions;
+import org.rdfhdt.hdt.util.BitUtil;
+import org.rdfhdt.hdt.util.Mutable;
+import org.rdfhdt.hdt.util.crc.CRC32;
+import org.rdfhdt.hdt.util.crc.CRC8;
+import org.rdfhdt.hdt.util.crc.CRCInputStream;
+import org.rdfhdt.hdt.util.io.IOUtil;
+import org.rdfhdt.hdt.util.string.ByteStringUtil;
+import org.rdfhdt.hdt.util.string.CompactString;
+import org.rdfhdt.hdt.util.string.ReplazableString;
+
+/**
+ * @author delfina.ramos
+ *
+ */
+public class JNIDictionarySectionBig implements DictionarySectionPrivate {
+	
+	static {
+		try {
+			System.loadLibrary("jnidictionary");
+		} catch (Exception e) {
+			throw new RuntimeException("Unable to load shared library for JNIDictionary", e);
+		}
+	}
+
+	public static String JNIDic;
+	
+	public static final int TYPE_INDEX = 2;
+	public static final int DEFAULT_BLOCK_SIZE = 16;
+	public static final int BLOCK_PER_BUFFER = 1000000;
+	
+	// FIXME: Due to java array indexes being int, only 2GB can be addressed per dictionary section.
+	protected byte [][] text; // Encoded sequence
+	protected long [] postFirst;
+	
+	protected int blocksize;
+	protected int numstrings;
+	protected SequenceLog64Big blocks;
+	protected String fname;
+	protected long jnidictionary;
+	
+	public static byte[] toByteArray(CharSequence charSequence) {
+		if (charSequence == null) {
+	    	return null;
+	    }
+	    byte[] barr = new byte[charSequence.length()];
+	    for (int i = 0; i < barr.length; i++) {
+	    	barr[i] = (byte) charSequence.charAt(i);
+	    }
+	    return barr;
+	}
+	
+	public JNIDictionarySectionBig(HDTOptions spec) {
+		this.blocksize = (int) spec.getInt("jni.blocksize");
+		if(blocksize==0) {
+			blocksize = DEFAULT_BLOCK_SIZE;
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see hdt.dictionary.DictionarySection#load(hdt.dictionary.DictionarySection)
+	 */
+	@Override
+	public void load(TempDictionarySection other, ProgressListener listener, String dict) throws IOException {
+		this.blocks = new SequenceLog64(BitUtil.log2(other.size()), other.getNumberOfElements()/blocksize);
+		Iterator<? extends CharSequence> it = other.getSortedEntries();
+		this.load((Iterator<CharSequence>)it, other.getNumberOfElements(), listener, dict);
+	}
+
+	public void load(Iterator<CharSequence> it, long numentries, ProgressListener listener, String dict) throws IOException {
+		this.blocks = new SequenceLog64(32, numentries/blocksize);
+		this.numstrings = 0;
+		
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();  
+
+		while(it.hasNext()) {
+			CharSequence str = it.next();
+			byte[] barr = toByteArray(str);
+						
+			outputStream.write(barr);	
+			outputStream.write(0);
+			
+			numstrings++;
+		}
+
+		text = outputStream.toByteArray();
+		System.out.println("Max memory dictionary(bytes): " + 
+				  Runtime.getRuntime().maxMemory());
+		System.out.println("Free memory dictionary(bytes): " + 
+				  Runtime.getRuntime().freeMemory());
+
+		System.out.println("length of byte array : "+text.length);
+		
+		jnidictionary = _createJNIDictionary(text, blocksize, dict);
+
+	}
+	
+	@Override
+	public void load(String filename, ProgressListener listener) throws IOException {
+			
+		jnidictionary = _loadJNIDictionary(filename);
+		numstrings=getNElements(jnidictionary);
+		
+	}
+		
+	protected int locateBlock(CharSequence str) {
+		if(blocks.getNumberOfElements()==0) {
+			return -1;
+		}
+		
+		int low = 0;
+		int high = (int)blocks.getNumberOfElements()-1;
+		int max = high;
+		
+		while (low <= high) {
+			int mid = (low + high) >>> 1;
+			
+			int cmp;
+			if(mid==max) {
+				cmp = -1;
+			} else {
+				int pos = (int)blocks.get(mid);
+				cmp = ByteStringUtil.strcmp(str, text, pos);
+//				System.out.println("Comparing against block: "+ mid + " which is "+ ByteStringUtil.asString(text, pos)+ " Result: "+cmp);
+			}
+			
+			if (cmp<0) {
+				high = mid - 1;
+			} else if (cmp > 0) {
+				low = mid + 1;
+			} else {
+				return mid; // key found
+			}
+		}
+		return -(low + 1);  // key not found.
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see hdt.dictionary.DictionarySection#locate(java.lang.CharSequence)
+	 */
+	@Override
+	public int locate(CharSequence str) {
+		if (numstrings==0) {
+			return 0;
+		}
+		
+		int location = locate(str.toString(), str.length(), jnidictionary);
+		
+		return location;
+	}
+		
+	/* (non-Javadoc)
+	 * @see hdt.dictionary.DictionarySection#extract(int)
+	 */
+	@Override
+	public CharSequence extract(int id) {
+		if (numstrings==0 || id<1 || id>numstrings) {
+			return null;
+		}
+
+		String res = extract(id, jnidictionary);
+		
+		return res;
+	}
+	
+	/* (non-Javadoc)
+	 * @see hdt.dictionary.DictionarySection#load(java.io.InputStream, hdt.ProgressListener)
+	 */
+	@Override
+	public void load(InputStream input, ProgressListener listener) throws IOException {
+		
+		//todo esto no hace falta porque lo hago en c, llama a método nativo
+		CRCInputStream in = new CRCInputStream(input, new CRC8());
+		
+		// Read type
+		int type = in.read();
+		if(type!=TYPE_INDEX) {
+			throw new IllegalFormatException("Trying to read a DictionarySectionPFC from data that is not of the suitable type");
+		}
+		
+		// Read vars
+		numstrings = (int) VByte.decode(in);
+		long bytes = VByte.decode(in);
+		blocksize = (int) VByte.decode(in);		
+	
+		if(!in.readCRCAndCheck()) {
+			throw new CRCException("CRC Error while reading Dictionary Section Plain Front Coding Header.");
+		}
+		
+		if(bytes>Integer.MAX_VALUE) {
+			input.reset();
+			throw new IllegalArgumentException("This class cannot process files with a packed buffer bigger than 2GB"); 
+		}
+		
+		// Read blocks
+		blocks = new SequenceLog64();
+		blocks.load(input, listener);	// Read blocks from input, they have their own CRC check.
+		
+		// Read packed data
+		in.setCRC(new CRC32());
+		text = IOUtil.readBuffer(in, (int) bytes, listener);
+		if(!in.readCRCAndCheck()) {
+			throw new CRCException("CRC Error while reading Dictionary Section Plain Front Coding Data.");
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		text=null;
+		if (blocks!=null) blocks.close();
+		blocks=null;
+	}
+
+	@Override
+	public long size() {
+		return text.length+blocksize;
+	}
+
+	@Override
+	public int getNumberOfElements() {
+		int numero = getNElements(jnidictionary);
+		return numero;
+	}
+
+	@Override
+	public Iterator<? extends CharSequence> getSortedEntries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(OutputStream output, String filename, ProgressListener listener) throws IOException {
+		
+		_saveJNIDictionary(jnidictionary, filename);
+		System.out.println(filename+" saved successfully");
+		
+	}
+	
+	protected native String _writeJNIDictionary(String filename);
+	protected native long _createJNIDictionary(byte [] it, int bucketsize, String dict);
+	protected native int locate(String str, int strLen, long jnidictionary);
+	protected native String extract(int id, long jnidictionary);
+	protected native long _loadJNIDictionary(InputStream in);
+	protected native long _loadJNIDictionary(String filename);
+	protected native void _saveJNIDictionary(long jnidictionary, String filename);
+	protected native int getNElements(long jnidictionary);
+
+	@Override
+	public void load(TempDictionarySection other, ProgressListener listener) throws IOException {
+				
+	}
+
+	@Override
+	public void save(OutputStream output, ProgressListener listener) throws IOException {
+		
+	}
+	
+
+}
